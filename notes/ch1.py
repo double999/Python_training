421f4df0fdc86037b5a5c113c735f009431f8822
double999 <double99@tju.edu.cn>
2019-08-21 13:47:22 +0800

    初始化

---
+++ notes/ch1.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+# python training records
+
+""" ch1 快速上手 基础知识
+# 
+#
+#
+"""
+1 / 2      # 浮点运算，结果为0.5，python3特性
+1 // 2     # 整除运算，结果为0，舍弃小数部分 向下取整；对于负数，越整到离0更远
+2 ** 3     # 乘方运算符 也可使用内置函数 pow()
+0xAF       # 16进制表示 175
+0b10       # 二进制表示
+##
+print('I am the world')
+print(4*4)
+##
+input("the number is : ")
+a= input("the number a is : ")
+b= input("the number b is : ")
+print(int(a)*int(b))  #类似于类型转换
+
